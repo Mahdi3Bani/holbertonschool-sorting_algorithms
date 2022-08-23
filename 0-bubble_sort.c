e39d2d4c377e03ee953430f87d428b0110ed6c01
Mahdi3Bani <bani.mahdi666@gmail.com>
2022-08-23 20:50:09 +0100

    bubble

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,18 +1,16 @@
 #include "sort.h"
-
-
-
-
-
-
-void swap(int* xp, int* yp)
+/**
+ * swap - swap items
+ *
+ * @xp: fiest item
+ * @yp: next item
+ */
+void swap(int *xp, int *yp)
 {
-    int temp = *xp;
-    *xp = *yp;
-    *yp = temp;
+	int temp = *xp;
+	*xp = *yp;
+	*yp = temp;
 }
-
-
 
 /**
  * bubble_sort - sorts an array of integers in ascending order using
@@ -25,18 +23,22 @@ void bubble_sort(int *array, size_t size)
 	size_t i;
 	int check = 1, test;
 
+	if (array == NULL)
+	{
+		return;
+	}
+
 	while (check)
 	{
 		test = 0;
 		for (i = 0; i < size - 1; i++)
-		{	
-			if (array[i] > array  [i + 1])
+		{
+			if (array[i] > array[i + 1])
 			{
 				swap(&array[i], &array[i + 1]);
 				print_array(array, size);
 				test++;
 			}
-
 		}
 		if (test == 0)
 		{
