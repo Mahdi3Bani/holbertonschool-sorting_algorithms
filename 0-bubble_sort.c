894b90e16dfbbe5eb0695f00af65fa87d2296a45
Mahdi3Bani <bani.mahdi666@gmail.com>
2022-08-23 20:24:21 +0100

    fo9a3at

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -29,7 +29,7 @@ void bubble_sort(int *array, size_t size)
 	{
 		if (array[j] > array[j + 1])
 		{
-			swap(&array[i], &array[j]);
+			swap(&array[i], &array[j + 1]);
 		}
 		print_array(array, size);
 	}
