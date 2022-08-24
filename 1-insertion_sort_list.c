bf83dc2642fa0b8cd8bd9405124dafc0ecca14b5
Mahdi3Bani <bani.mahdi666@gmail.com>
2022-08-24 17:30:18 +0100

    insertion

---
+++ 1-insertion_sort_list.c
@@ -0,0 +1,31 @@
+#include "sort.h"
+/**
+ * swapp - swap
+ * @prev: head of the node
+ * 
+ */
+
+
+
+
+/**
+ * @insertion_sort - sorts a doubly linked list of integers
+ * in ascending order using the Insertion sort algorithm
+ * list: double linked llist
+ */
+
+void insertion_sort_list(listint_t **list)
+{
+   
+listint_t *node;
+
+
+node = malloc(sizeof(listint_t));
+if (!list)
+return;
+
+while (list)
+{
+    if ()
+}
+}
