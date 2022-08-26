c4ff6e6f1d959868ef50f6a552f6ece49bae7d09
dhaferHS <dhafer.darkprod@gmail.com>
2022-08-26 10:45:31 +0100

    3-quick_sort.c

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -1,4 +1,74 @@
 #include "sort.h"
+
+/**
+ * quick_sort - sort an array of ints in ascending order (QuickSort)
+ * @array: array of integers
+ * @size: number of elements in array
+ *
+ * Description: implementing Lomuto partition scheme
+ * Return: 0
+ */
+void quick_sort(int *array, size_t size)
+{
+	if (!array || size < 2)
+		return;
+	quicker_sort(array, size, 0, size - 1);
+}
+
+/**
+ * quicker_sort - does the actual recursive QuickSort
+ * @array: array of integers
+ * @size: number of elements in array
+ * @l: first index of array
+ * @h: last index of array
+ *
+ * Return: 0
+ */
+void quicker_sort(int *array, size_t size, int l, int h)
+{
+	int i;
+
+	if (l < h)
+	{
+		i = partition(array, size, l, h);
+		quicker_sort(array, size, l, i - 1);
+		quicker_sort(array, size, i + 1, h);
+	}
+}
+/**
+ * partition - partition through an array of integers
+ * @array: array of integers
+ * @size: number of elements of array
+ * @l: first index of array
+ * @h: last index of array
+ *
+ * Description: all values must be accurate
+ * Return: new index position
+ */
+int partition(int *array, size_t size, int l, int h)
+{
+	int pivot = array[h], i = l, j;
+
+	for (j = l; j <= h - 1; j++)
+	{
+		if (array[j] < pivot)
+		{
+			if (i != j)
+			{
+				swap(&array[i], &array[j]);
+				print_array(array, size);
+			}
+			i++;
+		}
+	}
+	if (pivot != array[i])
+	{
+		swap(&array[i], &array[h]);
+		print_array(array, size);
+	}
+	return (i);
+}
+
 /**
  * swap - swap
  * @a: int to swap
@@ -12,66 +82,3 @@ void swap(int *a, int *b)
 	*b = t;
 }
 
-/**
- * partition - find the partition position
- * @array: array
- * @low: lowest index
- * @high: highest index
- * @size: size of array
- * Return: partition point
- */
-int partition(int *array, int low, int high, size_t size)
-{
-	int j;
-
-	int pivot = array[high];
-
-	int i = (low - 1);
-
-	for (j = low; j < high; j++)
-	{
-		if (array[j] <= pivot)
-		{
-
-			i++;
-
-			swap(&array[i], &array[j]);
-				}
-	}
-
-	swap(&array[i + 1], &array[high]);
-	print_array(array, size);
-
-	return (i + 1);
-}
-
-/**
- * quicksort - quicksort
- * @array: array
- * @low: lowest index
- * @high: highest index
- * @size: size of array
- */
-void quicksort(int *array, int low, int high, size_t size)
-{
-	if (low < high)
-	{
-
-		int pi = partition(array, low, high, size);
-
-		quicksort(array, low, pi - 1, size);
-		quicksort(array, pi + 1, high, size);
-	}
-}
-
-/**
- * quick_sort - quick sort
- * @array: array
- * @size: size of array
- *
- */
-void quick_sort(int *array, size_t size)
-{
-
-	quicksort(array, 0, size - 1, size);
-}
